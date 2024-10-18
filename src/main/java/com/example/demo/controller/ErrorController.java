691df36c7aefefa0cbd7aeec7595dbbae4306844
santos <joaosantos@solides.com.br>
2024-10-18 15:31:43 -0300

    fix

---
+++ src/main/java/com/example/demo/controller/ErrorController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ErrorController {
+
+    /**
+     * PADRONIZE OS RETORNOS DE EXCEPTIONS DO SEU PROJETO
+     * */
+
+}
--- src/main/java/com/example/demo/controller/exception/ServerErrorException.java
+++ src/main/java/com/example/demo/controller/exception/ServerErrorException.java
@@ -1,4 +1,4 @@
-package com.example.demo.exception;
+package com.example.demo.controller.exception;
 
 
 import org.springframework.http.HttpStatus;
